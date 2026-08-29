b9d9205437839f57cb22684240031b1e18f7b9fd
Nikhil Kumar Singh <nikhilkumarsingh.in@gmail.com>
2026-08-29 18:35:06 +0530

    chore(application): add express server

---
+++ src/application.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+
+import { EnvironmentVariables } from "./configuration/environment-variables.configuration.ts";
+
+export const application = express();
+
+application.use(express.json({ limit: "1mb" }));
+application.use(express.urlencoded({ extended: true, limit: "1mb" }));
+application.use(cors());
+application.use(helmet());
+application.use(compression({ level: 6, threshold: 512 }));
+application.use(morgan(EnvironmentVariables.CURRENT_ENVIRONMENT === "production" ? "combined" : "dev"));
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { Server as HttpServer } from "node:http";
+
+import { application } from "./application.ts";
+import { EnvironmentVariables } from "./configuration/environment-variables.configuration.ts";
+
+const server = new HttpServer(application);
+
+server.listen(EnvironmentVariables.PORT, () => {
+    console.log("Server is listening on port:", EnvironmentVariables.PORT);
+    console.log("Current environment for the application is:", EnvironmentVariables.CURRENT_ENVIRONMENT);
+});
