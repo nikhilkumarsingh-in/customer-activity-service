d6675e47d1d4610d62305c77eaa1e91ecddcf49c
Nikhil Kumar Singh <nikhilkumarsingh.in@gmail.com>
2026-09-08 19:02:52 +0530

    refactor(customer): remove rate limit middleware from patch routes

--- src/modules/customers/customer.routes.ts
+++ src/modules/customers/customer.routes.ts
@@ -24,22 +24,8 @@ export class CustomerRoutes {
         this.router.get("/", this.controller.search.bind(this.controller));
         this.router.get("/:id", this.controller.details.bind(this.controller));
 
-        this.router.patch(
-            "/:id",
-            handleCreateCustomRateLimitMiddlewareForRoute(10),
-            this.controller.updateDetails.bind(this.controller)
-        );
-
-        this.router.patch(
-            "/:id/role",
-            handleCreateCustomRateLimitMiddlewareForRoute(5),
-            this.controller.updateRole.bind(this.controller)
-        );
-
-        this.router.patch(
-            "/:id/status",
-            handleCreateCustomRateLimitMiddlewareForRoute(5),
-            this.controller.updateStatus.bind(this.controller)
-        );
+        this.router.patch("/:id", this.controller.updateDetails.bind(this.controller));
+        this.router.patch("/:id/role", this.controller.updateRole.bind(this.controller));
+        this.router.patch("/:id/status", this.controller.updateStatus.bind(this.controller));
     }
 }
